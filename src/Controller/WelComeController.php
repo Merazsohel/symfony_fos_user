fa5f9e267f6a584507cd800ad20f1637a537974b
meraz3308 <merazhossain64@gmail.com>
2019-10-23 15:01:16 +0600

    login,register,reset password,email,group

---
+++ src/Controller/WelComeController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WelComeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+
+
+
+
+}
